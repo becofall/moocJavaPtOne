0b374f3c3a429a92960f0cbb5e16ab780e2bb660
becofall <84538196+becofall@users.noreply.github.com>
2021-05-20 13:45:42 -0500

    Add files via upload

---
+++ part05-Part05_14.Books/src/main/java/Book.java
@@ -0,0 +1,25 @@
+
+public class Book {
+
+    private String name;
+    private int publicationYear;
+
+    public Book(String name, int publicationYear) {
+        this.name = name;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    public boolean equals(Object compared) {
+        Book comparedBook = (Book) compared;
+        return (this.name.equals(comparedBook.name) &&
+            this.publicationYear == comparedBook.publicationYear); 
+    }
+
+}
